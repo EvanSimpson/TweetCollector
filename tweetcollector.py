00cc69cfb06e0834587d1e411e6e618aadce9ae1
Evan Simpson <evan.simpson@students.olin.edu>
2012-10-10 13:10:13 -0400

    Adding final version of tweepy implementation for collecting tweets.

---
+++ tweetcollector.py
@@ -0,0 +1,34 @@
+import tweepy
+from textwrap import TextWrapper
+import pymongo as mongo
+
+
+
+
+auth1 = tweepy.auth.OAuthHandler(AuthVars.c_key, AuthVars.c_secret)
+auth1.set_access_token(AuthVars.a_token, AuthVars.a_secret)
+api = tweepy.API(auth1)
+
+
+class StreamListener(tweepy.StreamListener):
+    status_wrapper = TextWrapper(width=60, initial_indent='    ', subsequent_indent='    ')
+    connection = mongo.Connection(' mongodb://heroku_app8272532:q30abjchsqvvl19k1cuhsq6eib@ds039027.mongolab.com:39027/heroku_app8272532')
+    db = connection.heroku_app8272532
+    tweets_collection = db.tweets
+    def on_status(self, status):
+        try:
+            tweets_collection.insert(status.__dict__.iteritems())
+            print self.status_wrapper.fill(status.text)
+            print '\n %s  %s  via %s\n' % (status.author.screen_name, status.created_at, status.source)
+        
+        except Exception, e:
+            # Catch any unicode errors while printing to console
+            # and just ignore them to avoid breaking application.
+            pass
+
+
+l = StreamListener()
+streamer = tweepy.Stream(auth=auth1, listener=l, timeout=3000000000 )
+setTerms = ["2012 election","presidential election","presidential debate","US president","obama administration","president of the united states","republican","ron paul","mitt romney","mitt","romney","democrat","democrats","democratic","barack obama","obama"]
+bbox = ['-180','-90','180','90']
+streamer.filter(None, setTerms, locations = bbox)
